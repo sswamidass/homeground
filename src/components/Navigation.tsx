7bcc95e2dd3c634b7e34b23eea4a32e59e912163
sswamidass <swamidass.sanjay@gmail.com>
2025-11-18 11:45:06 -0600

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -26,12 +26,7 @@ function Navigation({ isTransparent = false }: NavigationProps) {
     <nav className={`navigation ${isTransparent ? 'transparent' : ''}`}>
       <div className="nav-container">
         <Link to="/" className="logo" onClick={closeMenu}>
-          <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M20 5C11.716 5 5 11.716 5 20C5 28.284 11.716 35 20 35C28.284 35 35 28.284 35 20C35 11.716 28.284 5 20 5Z" fill="#4a90c7"/>
-            <path d="M20 10C14.477 10 10 14.477 10 20C10 25.523 14.477 30 20 30C25.523 30 30 25.523 30 20C30 14.477 25.523 10 20 10Z" fill="#7eb3d8"/>
-            <path d="M25 17C25 14.791 23.209 13 21 13H19C16.791 13 15 14.791 15 17V23C15 25.209 16.791 27 19 27H21C23.209 27 25 25.209 25 23V17Z" fill="#1e5a7d"/>
-          </svg>
-          <span className="logo-text">Homeground</span>
+          <img src="/logo.png" alt="Homeground Coffee" style={{ height: '40px', width: 'auto' }} />
         </Link>
 
         <button
