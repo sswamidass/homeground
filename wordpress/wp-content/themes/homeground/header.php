2e6ceeb3db05f0d795e401e85fef5b6b67c40ca3
Sanjay Swamidass <swamidass.sanjay@gmail.com>
2025-12-05 10:46:14 -0600

    Update homeground project

--- wordpress/wp-content/themes/homeground/header.php
+++ wordpress/wp-content/themes/homeground/header.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
